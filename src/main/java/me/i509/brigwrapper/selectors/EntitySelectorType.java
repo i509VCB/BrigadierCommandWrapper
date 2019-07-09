6b799d623c79b1f44860eeda90df77ef6724ac01
i509VCB <null>
2019-07-08 23:20:34 -0500

    Clean temp

--- src/main/java/me/i509/brigwrapper/selectors/EntitySelectorType.java
+++
@@ -1,5 +0,0 @@
-package me.i509.brigwrapper.selectors;
-
-public enum EntitySelectorType {
-    MANY_ENTITIES, MANY_PLAYERS, ONE_ENTITY, ONE_PLAYER
-}
--- src/main/java/me/i509/brigwrapper/selectors/SuggestionProviderEnum.java
+++
@@ -1,6 +0,0 @@
-package me.i509.brigwrapper.selectors;
-
-public enum SuggestionProviderEnum {
-    DYNAMIC_STRING
-
-}
